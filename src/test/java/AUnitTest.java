4b39c2648516c238371ecfcf4aa4a54aafdb9117
OlyaTelesh <telesh.olya@mail.ru>
2022-09-28 18:49:12 +0300

    testng

---
+++ src/test/java/AUnitTest.java
@@ -0,0 +1,8 @@
+import org.testng.annotations.Test;
+
+public class AUnitTest extends BaseTest{
+    @Test
+    public void firstTest() {
+        System.out.println("Hello world");
+    }
+}
---
+++ src/test/java/BUnitTest.java
@@ -0,0 +1,8 @@
+import org.testng.annotations.Test;
+
+public class BUnitTest extends BaseTest{
+    @Test
+    public void secondTest() {
+        System.out.println("Hello world");
+    }
+}
